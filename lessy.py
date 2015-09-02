687d47c71bca0f958c0b2aec199e5aab244933bf
Fabio Fuschi <fabio@snk-interactive.de>
2015-09-02 10:50:43 +0200

    fix lessc call for multi plattform support

--- lessy.py
+++ lessy.py
@@ -1,5 +1,7 @@
 import sublime, sublime_plugin
 import subprocess
+import platform
+import os
 #
 # view.run_command('lessy')
 class LessyCommand(sublime_plugin.TextCommand):
@@ -7,7 +9,24 @@ class LessyCommand(sublime_plugin.TextCommand):
 		sels = self.view.sel()
 		for sel in sels:
 			bla = self.view.substr(sel)  
-			p =subprocess.Popen(["echo '"+bla+"' | /usr/local/bin/lessc -"], stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
+
+			lessc_command = "lessc"
+			#
+			if platform.system() == 'Windows':
+				# change command from lessc to lessc.cmd on Windows,
+				# only lessc.cmd works but lessc doesn't
+				lessc_command = 'lessc.cmd'
+			else:
+				# if is not Windows, modify the PATH
+				env = os.getenv('PATH')
+				env = env + ':/usr/bin:/usr/local/bin:/usr/local/sbin'
+				os.environ['PATH'] = env
+				# check for the existance of the less compiler, exit if it can't be located
+				if subprocess.call(['which', lessc_command]) == 1:
+					return sublime.error_message('lessy error: `lessc` is not available')
+
+
+			p =subprocess.Popen(["echo '"+bla+"' | "+lessc_command+" -"], stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
 			output, stderr = p.communicate()
 
 			if (stderr):
